7f50ebef19d74f4309f098612e80b8a1e17ae07c
Kawin Nikomborirak <concavemail@gmail.com>
2018-06-14 18:05:48 +0000

    Add incomplete test

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,15 +3,25 @@ import           FRP.Yampa
 import           Hedgehog
 import qualified Hedgehog.Gen   as Gen
 import qualified Hedgehog.Range as Range
+import           Network
+import           SDL
+import           Types
 
 prop_rectWithin :: Property
 prop_rectWithin = property $ do
   -- pY <- double (Range.linearFrac -100 100)
-  let r1 = -100
-      r2 = 100
-  r <-  (,) <$> forAll (Gen.double (Range.linearFrac r1 r2)) <*> forAll (Gen.double (Range.linearFrac r1 r2))
+  let mG = forAll (Gen.double (Range.linearFrac (-100) 100))
+  outer <- newRect mG mG mG mG
+  let p = outer ^. rectP
+      px = p ^. _x
+      py = p ^. _y
+      x = forAll (Gen.double (Range.linearFrac px (px + outer ^. rectD . _x)))
+      y = forAll (Gen.double (Range.linearFrac px (py + outer ^. rectD . _y)))
+  inner <- newRect x y mG mG
+  assert $ rectCollide outer inner
 
   pure ()
+  where newRect mpx mpy mdx mdy = Rectangle <$> (P <$> (V2 <$> mpx <*> mpy)) <*> (V2 <$> mdx <*> mdy)
   -- pX <- forAll $ Gen.List (Range.linearFrac -100 100)
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
