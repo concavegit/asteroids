f43d953481dbbbc8c516ecfe2ae31a71dda46f61
Kawin Nikomborirak <concavemail@gmail.com>
2018-06-25 04:12:48 +0000

    Shorten asteroidBelt to asters

--- src/Network.hs
+++ src/Network.hs
@@ -21,11 +21,11 @@ network gen game = proc ctrl -> do
         vMult = (view asteroidVMult . either id id . head) generate <$ astersEnd
     accel <- astersAccel (view asteroidV . either id id . head $ game ^. gameAsteroidBelt) -< (generate, vMult)
 
-    forward <- asteroidBeltForward (game ^. gameAsteroidBelt) >>> iPre (game ^. gameAsteroidBelt) -< accel
+    forward <- astersForward (game ^. gameAsteroidBelt) >>> iPre (game ^. gameAsteroidBelt) -< accel
     offset <- accumHold 0 -< (+ ((^. asteroidSprite . spriteRect . rectD . _x) . either id id . head) forward) . (+ game ^. gameBounds . _x) <$ astersEnd
 
     let looped = eitherFmap (asteroidSprite . spriteRect . rectP . _x +~ offset) forward
-    astersEnd <- asteroidBeltAroundE . (^. asteroidSprite . spriteRect . rectP . _x)
+    astersEnd <- astersAroundE . (^. asteroidSprite . spriteRect . rectP . _x)
       . either id id . head $ game ^. gameAsteroidBelt
       -< game & gameAsteroidBelt .~ forward
 
@@ -40,8 +40,8 @@ network gen game = proc ctrl -> do
     >> gameScore . score .= score'
     ) game
 
-asteroidBeltForward :: AsteroidBelt -> SF AsteroidBelt AsteroidBelt
-asteroidBeltForward belt0 = proc belt -> do
+astersForward :: AsteroidBelt -> SF AsteroidBelt AsteroidBelt
+astersForward belt0 = proc belt -> do
   move <- integral >>^ (asteroidSprite . spriteRect . rectP . _x .~)
     . (+ either id id (head belt0) ^. asteroidSprite . spriteRect . rectP . _x)
     -< either id id (head belt) ^. asteroidV
@@ -53,8 +53,8 @@ gameBoundTraversed d = proc g -> do
   x <- edge -< (<= 0) . (+ y) . (^. asteroidSprite . spriteRect . rectD . _x) . either id id . head $ g ^. gameAsteroidBelt
   returnA -< dup $ g <$ x
 
-asteroidBeltAroundE :: Double -> SF Game (Event Game)
-asteroidBeltAroundE d = dSwitch (gameBoundTraversed d) $ asteroidBeltAroundE . (^. gameBounds . _x)
+astersAroundE :: Double -> SF Game (Event Game)
+astersAroundE d = dSwitch (gameBoundTraversed d) $ astersAroundE . (^. gameBounds . _x)
 
 astersAccel :: Double -> SF (AsteroidBelt, Event Double) AsteroidBelt
 astersAccel d = proc (a, e) -> do
